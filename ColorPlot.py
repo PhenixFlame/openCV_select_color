78b4485a0db3bb44ffb9e6cced6c36c47ac889a2
anfenix <anfenix@yandex-team.ru>
2021-03-07 15:27:26 +0300

    color plot

---
+++ ColorPlot.py
@@ -0,0 +1,36 @@
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib import colors
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+path_image = "/home/anfenix/DATA/GIT/OpenCV/DATA/TRAIN1/2021-03-03 16-25-15.JPG"
+
+nemo = cv2.imread(path_image)
+r, g, b = cv2.split(nemo)
+fig = plt.figure()
+axis = fig.add_subplot(1, 1, 1, projection="3d")
+
+pixel_colors = nemo.reshape((np.shape(nemo)[0]*np.shape(nemo)[1], 3))
+norm = colors.Normalize(vmin=-1.,vmax=1.)
+norm.autoscale(pixel_colors)
+pixel_colors = norm(pixel_colors).tolist()
+axis.scatter(r.flatten(), g.flatten(), b.flatten(), facecolors=pixel_colors, marker=".")
+axis.set_xlabel("Red")
+axis.set_ylabel("Green")
+axis.set_zlabel("Blue")
+plt.show()
+
+
+h, s, v = cv2.split(hsv_nemo)
+fig = plt.figure()
+axis = fig.add_subplot(1, 1, 1, projection="3d")
+
+
+hsv_nemo = cv2.cvtColor(nemo, cv2.COLOR_RGB2HSV)
+axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker=".")
+axis.set_xlabel("Hue")
+axis.set_ylabel("Saturation")
+axis.set_zlabel("Value")
+plt.show()
